98a17c182c4b2bc9a070453ca33538bb57f5c7de
hui-zhang-adabella <hui.zhang.adabella@qq.com>
2022-12-20 01:43:19 +0800

    新增文件io程序

--- cal_pi.c
+++ cal_pi.c
@@ -10,11 +10,14 @@
 int g_thread_num = 1;
 int g_pai_num = 56000;
 int g_mode = 1; /* mode 1: 顺序执行， mode 2: 多线程执行 */
-
+int g_file_num = 1;
+int g_file_size = (1024 * 1024 * 512);
 
 /*
  * total means how many numbers of pai to cal
  * f is a array
+ *
+ * 
  */
 int cal_pai(int total, long *f)
 {
@@ -74,15 +77,75 @@ void usage(int argc, char *argv[])
     "Options:\n"
     " -n    how many times to run or how many thread create\n"
     " -p    pai num, how many pai's value\n"
-    " -m    mode, 1 means subsequense run, 2 means multi-thread\n"
-    ""
+    " -m    mode, 1 means subsequense run cpu burst, 2 means multi-thread run cpu burst, 3 means subsequense run io burst, 4 means multi thread run io burst\n"
+    " -s    io file size, default 10M = 10*1024*1024"
+    " -h    help information"
+    "\n"
     , program_name);
 }
 
+#define TEST_IO_PATH "./test_io/"
 
+int do_file_io(char *file_path, int g_file_size)
+{
+    char content[512];
+    FILE *f = fopen(file_path, "wb+");
+    if (f == NULL ) {
+        printf("do_file_io open file error:%s", file_path);
+        exit(EXIT_FAILURE);
+    }
+    int write_size = 0;
+    int write_count = g_file_size/sizeof(content);
+    int i = 0;
+    for (i = 0; i < write_count; i++) {
+        memset(content, i, sizeof(content));
+        if (fwrite(content, 1, sizeof(content), f) != sizeof(content)) {
+            printf("do_file_io write error\n");
+            exit(EXIT_FAILURE);
+        }
+        fflush(f);
+    }
+    int last = g_file_size % sizeof(content);
 
-#define SUBSEQUENSE_MODE    1
-#define MULTI_THREAD_MODE   2
+    if (fwrite(content, 1, last, f) != last) {
+        printf("do_file_io write error\n");
+        exit(EXIT_FAILURE);
+    }
+    fflush(f);
+    fclose(f);
+    
+    f = fopen(file_path, "wb+");
+    if (f == NULL) {
+        printf("do_file_io open file error:%sn", file_path);
+        exit(EXIT_FAILURE);
+    }
+    while(!feof(f)) {
+        size_t read_len = fread(content, sizeof(char), sizeof(content), f);    
+        if (read_len != sizeof(content)) {
+            printf("read file end:%d\n", read_len);
+        }
+    }
+    fclose(f);
+}
+
+void *thread_do_file_io(void *arg)
+{
+    int thread_num = *((int *)arg);
+    pid_t pid = getpid();
+    char file_path[128];
+    snprintf(file_path, sizeof(file_path), "%s%d", TEST_IO_PATH, thread_num);
+    printf("file full path:%s\n", file_path); 
+    if (do_file_io(file_path, g_file_size) != 0) {
+        printf("error do file io in thread num:%d, pid:%u\n", thread_num, pid);
+        return NULL;
+    }  
+    return NULL;
+}
+
+#define SUBSEQUENSE_MODE        1
+#define MULTI_THREAD_MODE       2
+#define IO_SUBSEQUENSE_MODE     3
+#define IO_MULTI_THREAD_MODE    4
 
 void main(int argc, char *argv[])
 {
@@ -102,7 +165,7 @@ void main(int argc, char *argv[])
             printf("option mode:%s\n", optarg);
             g_mode = atoi(optarg);
             break;
-	case 'h':
+        case 'h':
             usage(argc, argv);
             exit(0);
         default:
@@ -130,7 +193,6 @@ void main(int argc, char *argv[])
     } else if (g_mode == MULTI_THREAD_MODE) {
         for (i = 0; i < g_thread_num; i++) {
             pthread_t tid;
-            char thread_num[128];
             
             if (pthread_create(&tid, NULL, (void*)thread_do_cal_pai, &i) != 0) {
                 printf("pthread_create error, thread_num:%d\n", i);
@@ -140,7 +202,33 @@ void main(int argc, char *argv[])
             pthread_join(tid, (void *)&rev);
         }       
         printf("\n create thread %d, run success\n", g_thread_num);
-    } else {
+    } else if (g_mode == IO_SUBSEQUENSE_MODE) {
+        for (i = 0; i < g_thread_num; i++) {
+            char file_path[128];
+            snprintf(file_path, sizeof(file_path), "%s%d", TEST_IO_PATH, i);
+            int ret = do_file_io(file_path, g_file_size);
+            if (ret != 0) {
+                printf("\n error run do_file_io\n");
+                exit(EXIT_FAILURE);
+            }
+        } 
+        printf("\nsubsequense run file io %d times\n", g_thread_num); 
+
+    } else if (g_mode == IO_MULTI_THREAD_MODE) {
+        for (i = 0; i < g_thread_num; i++) {
+            pthread_t tid;
+            
+            if (pthread_create(&tid, NULL, (void*)thread_do_file_io, &i) != 0) {
+                printf("pthread_create error, thread_num:%d\n", i);
+                exit(EXIT_FAILURE);
+            }
+            char *rev = NULL;
+            pthread_join(tid, (void *)&rev);
+        }       
+        printf("\n create io thread %d, run success\n", g_thread_num);
+    }
+    
+    else {
         printf("mode:%d unknown\n", g_mode);
         exit(EXIT_FAILURE);
     }  
