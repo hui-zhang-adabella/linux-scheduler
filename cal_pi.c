d5bdee0bb065f1d5cec9d0e6da4108c9ff67b5d8
hui-zhang-adabella <hui.zhang.adabella@qq.com>
2022-12-20 17:30:29 +0800

    add pthread sched function

--- cal_pi.c
+++ cal_pi.c
@@ -7,11 +7,53 @@
 #include <string.h>
 #include <getopt.h> 
 
+#include <unistd.h>
+#include <sched.h>
+#include <errno.h>
+
+
 int g_thread_num = 1;
 int g_pai_num = 56000;
 int g_mode = 1; /* mode 1: 顺序执行， mode 2: 多线程执行 */
 int g_file_num = 1;
-int g_file_size = (1024 * 1024 * 512);
+int g_file_size = (1024 * 1024 * 256);
+int g_sched = 0; /* 0:SCHED_OTHER, 1:SCHED_RR, 2:SCHED_FIFO  */
+
+int g_io_priority = 99;
+int g_cpu_priority = 98;
+/*
+ * set thread scheduler policy and priority
+ * policy are SCHED_FIFO, SCHED_RR, and SCHED_OTHER
+ * 
+ */
+static int pthread_set_sched_prolicy_and_priority(pthread_attr_t *attr, struct sched_param *param, int policy, int priority)
+{
+    int ret;
+    ret = pthread_attr_init(attr);
+    if (ret != 0) { 
+        printf("attr_init error is %s\n", strerror(ret));
+        return ret;
+    }
+    param->sched_priority = priority;
+    ret = pthread_attr_setinheritsched(attr, PTHREAD_EXPLICIT_SCHED);
+    if (ret != 0) {
+        printf("set inheritsched error is %s\n", strerror(errno));
+        return ret;
+    }
+    ret = pthread_attr_setschedpolicy(attr, policy);
+    if (ret != 0) {
+        printf("setschedpolicy error is %s\n", strerror(errno));
+        return ret;
+    }
+
+    ret = pthread_attr_setschedparam(attr, param);
+    if (ret) {
+        printf("setschedpolicy error is %s, ret = %d\n, you may need sudo to run", strerror(errno), ret);
+        return ret;
+    }
+    
+    return 0;
+}
 
 /*
  * total means how many numbers of pai to cal
@@ -77,9 +119,11 @@ void usage(int argc, char *argv[])
     "Options:\n"
     " -n    how many times to run or how many thread create\n"
     " -p    pai num, how many pai's value\n"
-    " -m    mode, 1 means subsequense run cpu burst, 2 means multi-thread run cpu burst, 3 means subsequense run io burst, 4 means multi thread run io burst\n"
-    " -s    io file size, default 10M = 10*1024*1024"
-    " -h    help information"
+    " -m    mode, 1 means subsequense run cpu burst, 2 means multi-thread run cpu burst, 3 means subsequense run io burst, 4 means multi thread run io burst, 5 means multi thread run io burst and cpu burst,echo type half of total threads\n"
+    " -s    sched_mode, 0:SCHED_OTHER, 1:SCHED_RR, 2:SCHED_FIFO \n"
+    " -c    cpu burst priority, must use with SCHED_RR or SCHED_FIFO, value range [1, 99]\n"
+    " -i    io burst priority, must use with SCHED_RR or SCHED_FIFO, value range [1, 99]\n"
+    " -h    help information\n"
     "\n"
     , program_name);
 }
@@ -114,7 +158,7 @@ int do_file_io(char *file_path, int g_file_size)
     fflush(f);
     fclose(f);
     
-    f = fopen(file_path, "wb+");
+    f = fopen(file_path, "r");
     if (f == NULL) {
         printf("do_file_io open file error:%sn", file_path);
         exit(EXIT_FAILURE);
@@ -124,6 +168,13 @@ int do_file_io(char *file_path, int g_file_size)
         if (read_len != sizeof(content)) {
             printf("read file end:%d\n", read_len);
         }
+    }
+    fclose(f);
+
+    //delete file
+    f = fopen(file_path, "wb+");
+    if (f == NULL) {
+        exit(EXIT_FAILURE);
     }
     fclose(f);
 }
@@ -146,12 +197,14 @@ void *thread_do_file_io(void *arg)
 #define MULTI_THREAD_MODE       2
 #define IO_SUBSEQUENSE_MODE     3
 #define IO_MULTI_THREAD_MODE    4
+#define CPU_AND_IO_MIX_MODE     5
+
 
 void main(int argc, char *argv[])
 {
 
     int ch = 0;
-    while ((ch = getopt(argc, argv, "hn:p:m:")) != -1) {
+    while ((ch = getopt(argc, argv, "hn:p:m:s:c:i:")) != -1) {
         switch (ch) {
         case 'n':
             printf("option thread num:%s\n", optarg);
@@ -165,6 +218,18 @@ void main(int argc, char *argv[])
             printf("option mode:%s\n", optarg);
             g_mode = atoi(optarg);
             break;
+        case 's':
+            printf("option scheduler:%s\n", optarg);
+            g_sched =  atoi(optarg);
+            break;
+        case 'c':
+            printf("option cpu burst priority:%s\n", optarg);
+            g_cpu_priority = atoi(optarg);
+            break;
+        case 'i':
+            printf("option io burst priority:%s\n", optarg);
+            g_io_priority = atoi(optarg);
+            break;
         case 'h':
             usage(argc, argv);
             exit(0);
@@ -176,14 +241,25 @@ void main(int argc, char *argv[])
     
     }
 
+    int sched_value = SCHED_OTHER;
+    if (g_sched == 1) {
+        sched_value = SCHED_RR;
+    } else if (g_sched == 2) {
+        sched_value = SCHED_FIFO;
+    }
+    
 
+    pthread_attr_t attr;
+    struct sched_param param;
+    int ret = 0;
+    char *rev = NULL;
 
     clock_t startTime = clock(), endTime;
     
     int i = 0;
     if (g_mode == SUBSEQUENSE_MODE) {
         for (i = 0; i < g_thread_num; i++) {
-            int ret = do_cal_pai(g_pai_num);
+            ret = do_cal_pai(g_pai_num);
             if (ret != 0) {
                 printf("\n error run do_cal_pai\n");
                 exit(EXIT_FAILURE);
@@ -191,22 +267,37 @@ void main(int argc, char *argv[])
         }
         printf("\nsubsequense run %d times\n", g_thread_num); 
     } else if (g_mode == MULTI_THREAD_MODE) {
-        for (i = 0; i < g_thread_num; i++) {
-            pthread_t tid;
-            
-            if (pthread_create(&tid, NULL, (void*)thread_do_cal_pai, &i) != 0) {
-                printf("pthread_create error, thread_num:%d\n", i);
+        if (sched_value == SCHED_OTHER) {
+            for (i = 0; i < g_thread_num; i++) {
+                pthread_t tid;
+
+                if (pthread_create(&tid, NULL, (void*)thread_do_cal_pai, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+            }    
+        } else {
+            ret = pthread_set_sched_prolicy_and_priority(&attr, &param, sched_value, g_cpu_priority);
+            if (ret != 0) {
+                printf("set_sched_prolicy_and_priority, ret:%d, errno:%d", ret, errno);
                 exit(EXIT_FAILURE);
             }
-            char *rev = NULL;
-            pthread_join(tid, (void *)&rev);
-        }       
+            for (i = 0; i < g_thread_num; i++) {
+                pthread_t tid;
+                if (pthread_create(&tid, &attr, (void*)thread_do_cal_pai, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+            }
+        }
         printf("\n create thread %d, run success\n", g_thread_num);
     } else if (g_mode == IO_SUBSEQUENSE_MODE) {
         for (i = 0; i < g_thread_num; i++) {
             char file_path[128];
             snprintf(file_path, sizeof(file_path), "%s%d", TEST_IO_PATH, i);
-            int ret = do_file_io(file_path, g_file_size);
+            ret = do_file_io(file_path, g_file_size);
             if (ret != 0) {
                 printf("\n error run do_file_io\n");
                 exit(EXIT_FAILURE);
@@ -215,24 +306,93 @@ void main(int argc, char *argv[])
         printf("\nsubsequense run file io %d times\n", g_thread_num); 
 
     } else if (g_mode == IO_MULTI_THREAD_MODE) {
-        for (i = 0; i < g_thread_num; i++) {
-            pthread_t tid;
-            
-            if (pthread_create(&tid, NULL, (void*)thread_do_file_io, &i) != 0) {
-                printf("pthread_create error, thread_num:%d\n", i);
+        if (sched_value == SCHED_OTHER) {
+            for (i = 0; i < g_thread_num; i++) {
+                pthread_t tid;
+
+                if (pthread_create(&tid, NULL, (void*)thread_do_file_io, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+            }    
+        } else {
+            ret = pthread_set_sched_prolicy_and_priority(&attr, &param, sched_value, g_io_priority);
+             if (ret != 0) {
+                printf("set_sched_prolicy_and_priority, ret:%d, errno:%d", ret, errno);
                 exit(EXIT_FAILURE);
             }
-            char *rev = NULL;
-            pthread_join(tid, (void *)&rev);
-        }       
+
+             for (i = 0; i < g_thread_num; i++) { 
+                pthread_t tid;
+                if (pthread_create(&tid, &attr, (void*)thread_do_file_io, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+      
+        }
         printf("\n create io thread %d, run success\n", g_thread_num);
-    }
-    
-    else {
+    } else if (g_mode == CPU_AND_IO_MIX_MODE) {
+        if (sched_value == SCHED_OTHER) {
+            for (i = 0; i < g_thread_num/2; i++) {
+                pthread_t tid;
+                if (pthread_create(&tid, NULL, (void*)thread_do_file_io, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+            }
+
+            for (i = g_thread_num/2; i < g_thread_num; i++) {
+             
+                pthread_t tid;
+            
+                if (pthread_create(&tid, NULL, (void*)thread_do_cal_pai, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+
+                pthread_join(tid, (void *)&rev);
+            }
+        } else {
+            ret = pthread_set_sched_prolicy_and_priority(&attr, &param, sched_value, g_io_priority);
+
+            if (ret != 0) {
+                printf("set_sched_prolicy_and_priority, ret:%d, errno:%d", ret, errno);
+                exit(EXIT_FAILURE);
+            }
+
+            for (i = 0; i < g_thread_num/2; i++) { 
+                pthread_t tid;
+                if (pthread_create(&tid, &attr, (void*)thread_do_file_io, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+
+            }
+
+            ret = pthread_set_sched_prolicy_and_priority(&attr, &param, sched_value, g_cpu_priority);
+            if (ret != 0) {
+                printf("set_sched_prolicy_and_priority, ret:%d, errno:%d", ret, errno);
+                exit(EXIT_FAILURE);
+            }
+            for (i = g_thread_num/2; i < g_thread_num; i++) {
+                pthread_t tid;
+                if (pthread_create(&tid, &attr, (void*)thread_do_cal_pai, &i) != 0) {
+                    printf("pthread_create error, thread_num:%d\n", i);
+                    exit(EXIT_FAILURE);
+                }
+                pthread_join(tid, (void *)&rev);
+            }
+        }     
+    } else {
         printf("mode:%d unknown\n", g_mode);
         exit(EXIT_FAILURE);
     }  
 
     endTime = clock();
-    printf("\n time:%f\n", (double)(endTime - startTime));
+    //printf("\n time:%f\n", (double)(endTime - startTime));
 }
